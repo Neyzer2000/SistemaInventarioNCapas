0ef5873a7a2149a4d726603587ea2236ca166ccc
bryan2006rg <161053275+bryan2006rg@users.noreply.github.com>
2025-05-12 13:22:41 -0600

    SUBIENDO PARTE FINAL

---
+++ DAL/MiDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+public class MiDbContext : DbContext
+{
+    public DbSet<Producto> Productos { get; set; }
+
+    public MiDbContext() : base("miConexion")
+    {
+    }
+}
