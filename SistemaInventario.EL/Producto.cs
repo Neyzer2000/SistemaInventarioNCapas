eb9a6b5917f41ee6cbbce5b1b6a656931f08785e
bryan2006rg <161053275+bryan2006rg@users.noreply.github.com>
2025-05-13 19:26:29 -0600

    Ultimos Cambios!!

---
+++ SistemaInventario.EL/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaInventario.EL
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public int IdCategoria { get; set; }
+    }
+}
---
+++ SistemaInventario.EL/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaInventario.EL
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Rol { get; set; }
+    }
+}
